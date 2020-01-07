cd554412599a49bbf8d616af15d85ff90b2f7a39
forguo <forguo@dazwang.com>
2020-01-07 17:11:30 +0800

    feat: complete init

--- vue/js/vue.js
+++ vue/js/vue.js
@@ -1,52 +1,87 @@
-(function () {
+/**
+ * @Description 模拟实现一个简单的数据双向绑定
+ * @Author forguo
+ * @Date 2020/1/7
+ */
 
-    var vue = function () {
-        this.$em = document.getElementById('app');
-        this.$data = {
-            name: 'Hello World!'
-        };
+(function () {
+    'use strict';
+
+    const Vue = function (options) {
+        this.options = Object.assign({}, options);
+        this.$em = document.getElementById(this.options.el);
+        this.$data = this.options.data;
         this.init();
     };
 
-    vue.prototype.init = function () {
-        let currData = this.$data;
-        console.log('iiii');
-        Object.defineProperty(currData, 'name', {
-            get: function (target, key) {
-                console.log(target, key);
-                return target.key;
-            },
-            set: function (target, key, value) {
-                console.log(target, key, value);
-                target.key = value;
-            }
-        });
-
-        this.render();
+    Vue.prototype.init = function () {
+        let data = this.$data;
+        this.observe(data);
+        this.render(data.name);
         this.initEvent();
-
     };
 
-    // 数组的监听，使用了 伪装者模式
+    Vue.prototype.observe = function (data) {
+        if (!data || typeof data !== 'object') {
+            return false;
+        }
 
-    vue.prototype.init = function () {
-        this.render();
-        this.initEvent();
+        // 遍历data，将原来所有属性改成set和get的形式
+        // 先获取到数据的key和value
+        Object.keys(data).forEach((key) => {
+            if (typeof data[key] === 'object') {
+                this.observe(data[key]);
+            } else {
+                this.defineReactive(data, key, data[key]);
+            }
+        });
+    };
+
+    // 添加数据监听
+    // 由于Object.defineProperty只能作用于Object，
+    // 所以数组的监听，使用了伪装者模式
+    Vue.prototype.defineReactive = function (target, key, value) {
+        let that = this;
+        // ES5语法，不支持IE8及以下版本
+        Object.defineProperty(target, key, {
+            enumerable: true, // 可遍历
+            configurable: true, // 可删除
+            get() {
+                console.log('get', value);
+                return value;
+            },
+            set(newValue) {
+                console.log('set', newValue);
+                value = newValue;
+                // 数据改变，触发dom渲染
+                that.render(newValue);
+            }
+        });
     };
 
     // event
-    vue.prototype.initEvent = function () {
-        document.getElementById('btn').addEventListener('click', () => {
+    Vue.prototype.initEvent = function () {
+        document.getElementById('set').addEventListener('click', () => {
             this.$data.name = 'Hello Vue Data!';
-            this.render();
+        });
+        document.getElementById('get').addEventListener('click', () => {
+            console.log(this.$data);
         });
     };
 
     // 渲染dom
-    vue.prototype.render = function () {
-        this.$em.innerHTML = this.$data.name;
+    Vue.prototype.render = function (newValue) {
+        console.log('renderDom:', newValue);
+        this.$em.innerHTML = newValue;
     };
 
-    new vue();
-
+    new Vue({
+        el: 'app',
+        data: {
+            name: 'forguo',
+            grade: {
+                sex: 'man'
+            },
+        }
+    });
 })();
