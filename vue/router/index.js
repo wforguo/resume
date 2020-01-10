735e26b3f9e2f3bc760fabc194a02d0ecd51d69c
forguo <forguo@dazwang.com>
2020-01-10 16:35:07 +0800

    添加路由的js

---
+++ vue/router/index.js
@@ -0,0 +1,71 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+/**
+ * @Description 路由的自动加载的实现
+ * @Author forguo
+ * @Date 2020/1/10
+ */
+// const Home = resolve => {
+//     require.ensure(['@/views/Home'], () => {
+//         resolve(require('@/views/Home'))
+//     }, 'Home')
+// }
+// const About = resolve => {
+//     require.ensure(['@/views/About'], () => {
+//         resolve(require('@/views/About'))
+//     }, 'About')
+// }
+
+let routes = [
+//     {
+//     path: '/',
+//     name: 'Home',
+//     component: Home,
+// }, {
+//     path: '/About',
+//     name: 'About',
+//     component: About,
+// }
+];
+
+// 通过require.context函数获取一个特定的上下文
+/**
+ * 要搜索的文件夹目录
+ * 搜索它的子目录，
+ * 匹配文件的正则表达式。
+ */
+
+// require.context(
+//   directory: String,
+//   includeSubdirs: Boolean /* optional, default true */,
+//   filter: RegExp /* optional, default /^\.\/.*$/, any file */,
+//   mode: String  /* optional, 'sync' | 'eager' | 'weak' | 'lazy' | 'lazy-once', default 'sync' */
+// )
+
+let views = require.context('../views/', true, /\.vue$/);
+
+// 导出的方法有 3 个属性： resolve, keys, id。
+// - resolve 是一个函数，它返回请求被解析后得到的模块 id。
+// - keys 也是一个函数，它返回一个数组，由所有可能被上下文模块处理的请求组成。
+// - id 是上下文模块里面所包含的模块 id. 它可能在你使用 module.hot.accept 的时候被用到
+
+// 这里只用到 keys，返回搜索到的数组
+
+views.keys().forEach((key) => {
+    let routerName = views(key).default.name;
+    routes.push({
+        path: routerName === 'Home' ? '/' : `/${routerName}`,
+        title: routerName,
+        name: routerName,
+        component: views(key).default
+    });
+});
+
+// console.log(routes);
+
+Vue.use(Router);
+
+export default new Router({
+    routes
+});
---
+++ vue/views/components/About.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="home">
+
+        <p>Header</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'About',
+        props: {},
+        data() {
+            return {
+                items: ['a', 'b', 'c'],
+                list: [
+                    {
+                        name: 'www',
+                        gender: 'man',
+                    },
+                    {
+                        name: 'what',
+                        gender: 'woman'
+                    },
+                    {
+                        name: 'who',
+                        gender: 'woman'
+                    },
+                ]
+            }
+        },
+        mounted() {
+            console.log('about load');
+        },
+        created() {
+            console.log('about created');
+        },
+        methods: {
+            setAge(index) {
+                this.list[index].name = 2019 + index;
+                // this.$set(this.list[index], 'age', 2019 + index)
+                // this.$router.push({
+                //     name: 'Home'
+                // });
+            },
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    .upload-btn {
+        width: 200px;
+        margin: 0 auto;
+        height: 40px;
+        line-height: 40px;
+        background: #999;
+        border-radius: 5px;
+        position: relative;
+        cursor: pointer;
+        input {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            opacity: 0;
+            left: 0;
+            top: 0;
+        }
+    }
+
+    .item {
+        width: 100%;
+        height: 50px;
+        line-height: 50px;
+        box-shadow: 3px 2px 9px 0 #eee;
+        margin-bottom: 30px;
+        span {
+            margin-right: 10px;
+        }
+    }
+</style>
